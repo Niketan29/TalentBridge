d41afc0f4491d749372b4958e83cf31c29359189
Niketan29 <147406585+Niketan29@users.noreply.github.com>
2026-03-11 12:34:16 +0530

    updates

--- client/src/services/jobApi.js
+++ client/src/services/jobApi.js
@@ -1,7 +1,9 @@
 import api from "./api";
 
 export const getJobsApi = () => api.get("/jobs");
-export const getAllJobsApi = getJobsApi; 
+
+export const getAllJobsApi = (params = {}) =>
+  api.get("/jobs", { params });
 
 export const getJobByIdApi = (id) => api.get(`/jobs/${id}`);
 
